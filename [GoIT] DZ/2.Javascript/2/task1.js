5815dab756615000dd44cc2268fc9c62da2fa339
Sergey <sergey.v.tokareff@gmail.com>
2016-12-12 18:21:19 +0200

    a
    a

--- [GoIT] DZ/2.Javascript/2/task1.js
+++ [GoIT] DZ/2.Javascript/2/task1.js
@@ -6,7 +6,12 @@
 </head>
 <body>
     <script>
-        
+        var year = prompt('Какой сейчас год?');
+        if(year == 2016){
+            console.log('Вы правы!');
+        }else{
+            console.log('С луны свалился? 2016!!!');
+        }
     </script>
 </body>
 </html>
